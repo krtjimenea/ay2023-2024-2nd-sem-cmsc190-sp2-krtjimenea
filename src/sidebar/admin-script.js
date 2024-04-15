9db795c31e9f994670ff476139c162302224a749
krtjimenea <60125839+krtjimenea@users.noreply.github.com>
2024-04-15 21:46:44 +0800

    added course key for database, course rendering in UI

--- src/sidebar/admin-script.js
+++ src/sidebar/admin-script.js
@@ -4,7 +4,7 @@
 //import for SDKs
 import { FirebaseApp } from './firebase';
 import {getAuth,signInWithCredential,GoogleAuthProvider} from 'firebase/auth';
-import {getDatabase,ref,set,on, onValue, get, update,push, child, query,orderByChild,equalTo, orderByValue} from 'firebase/database';
+import {getDatabase,ref,set,on, onValue, get, update,push, child, query,orderByChild,equalTo, orderByValue,setValue} from 'firebase/database';
 //Initialize Firebase
 const auth = getAuth(FirebaseApp);
 //Initialize database
@@ -209,11 +209,11 @@ window.addEventListener('DOMContentLoaded', function () {
 
       //for clicking the faculty card
       if(target.id==='FacultyName'){
-        console.log('Clicked Faculty Card');
+        // console.log('Clicked Faculty Card');
         //get the clicked FacultyName or ID
         facultyNameValue = target.textContent;
         //call the function and pass the name value
-        viewDetailsFaculty();
+        viewDetailsFaculty(facultyNameValue);
 
       }
 
@@ -223,28 +223,87 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 //function to view the faculty details paneldf
-function viewDetailsFaculty(){
-  
+function viewDetailsFaculty(facultyNameValue){
+  console.log('Clicked Faculty Card');
+
   //manipulate the DOM
   // var currentHTML = document.getElementsByClassName("AppDiv-AdminManage");
   document.getElementsByClassName("AppDiv-AdminManage")[0].innerHTML ="";
   
-
+  
   //change the button
   // document.getElementsByClassName("AdminAddButton").innerHTML = '';
-  document.getElementsByClassName("AppDiv-AdminManage")[0].innerHTML = `<div class="AdminAddButton"> <button type="button" class="Add-Buttons-Admin" id="Add-New-Course">Add New Course</button> </div>`;
+  document.getElementsByClassName("AppDiv-AdminManage")[0].innerHTML = `<div class="AdminHeaderDetails">${facultyNameValue}</div><div class="AdminAddButton"> <button type="button" class="Add-Buttons-Admin" id="Add-New-Course">Add New Course</button> </div> <div id="cardList"></div>`;
 
+  viewDetailsCourse(facultyNameValue);
 
-  // //check if path is the admin view faculty then render the passed faculty name
-  // var currentFaculty = document.getElementById("FacultyName");
-  // currentFaculty.textContent = facultyNameValue;
   
 
 }
 
 //function to view the classes 
-function viewDetailsCourse(){
-  //manipulate AdminManageFaculty
+function viewDetailsCourse(facultyNameValue){
+  //manipulate AdminManageFaculty html panel
+  //manipulatr the DOM
+   //check if there is a logged in user
+   chrome.identity.getAuthToken({ interactive: true }, token =>
+    {
+      if ( chrome.runtime.lastError || ! token ) {
+        alert(`SSO ended with an error: ${JSON.stringify(chrome.runtime.lastError)}`)
+        return
+      }
+
+      //firebase authentication
+      signInWithCredential(auth, GoogleAuthProvider.credential(null, token))
+      .then(res =>{
+          const user = auth.currentUser;
+          //get profile uid
+          if (user !== null) {
+            const db = getDatabase(); 
+            const facultyRef = ref(db,'faculty-in-charge/');
+            onValue(facultyRef, (snapshot) => {
+              snapshot.forEach((childSnapshot) => {
+                const childKey = childSnapshot.key;
+                const childData = childSnapshot.val();
+                //each childData is already the object itself
+                if(childData.name === facultyNameValue){
+                  var cardListDiv = document.getElementById('cardList');
+                  //modify the card list div
+                
+                  cardListDiv.innerHTML='';
+                  //loop through the child snapshot
+                  for(const courses in childData.classes){
+          
+                    const course = childData.classes[courses];
+                    const courseCode = course.code;
+                    const courseTitle = course.title;
+                    // console.log("Course Key: " + courseCode);
+                    
+                      
+                    cardListDiv.innerHTML += `<div class="cards">
+                              <p class="cardHeader" id="FacultyName">${courseCode}</p>
+                                <div class="cardDivText">
+                                    <div class="cardSubDiv">
+                                        <p id="card-labels">Course Title:</p>
+                                        <p class="cardText" id="FacultyNumber">${courseTitle}</p>
+                                    </div>
+                              </div>
+                            </div>`;
+
+                  
+                    
+                  }
+                }
+              })//EOF forEach loop;
+            }, {
+              onlyOnce: true
+            });
+          }
+      })
+      .catch((err) => {
+        alert("SSO ended with an error" + err);
+      });
+  });
 }
 
 //function to create and save the new faculty
@@ -352,6 +411,7 @@ function createNewCourse(facultyNameValue){
                //get a db reference
                 const db = getDatabase();
                 const facultyRef = ref(db,'faculty-in-charge/');
+                
 
                 onValue(facultyRef, (snapshot) => {
                   snapshot.forEach((childSnapshot) => {
@@ -362,9 +422,9 @@ function createNewCourse(facultyNameValue){
                       alert("Success Firebase Access! " + childData.name + " FIC Exists");
                       //add the course
                       //new key for the course
-                      // const newCourseKey = push(child(ref(db), 'faculty-in-charge')).key;
+                      const newCourseKey = push(child(ref(db), `/faculty-in-charge/${childKey}/classes`)).key;
                       const updateCourse ={};
-                      updateCourse[`/faculty-in-charge/${childKey}/classes/class1/`] = newCourse;
+                      updateCourse[`/faculty-in-charge/${childKey}/classes/` + newCourseKey] = newCourse;
                       update(ref(db), updateCourse)
                         .then(()=>{
                           console.log('Success in Adding new course');
