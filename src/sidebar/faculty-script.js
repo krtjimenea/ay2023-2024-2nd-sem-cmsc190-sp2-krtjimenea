5150944bc0a17f2bf5ad54ef928dc3737b58820e
krtjimenea <60125839+krtjimenea@users.noreply.github.com>
2024-05-24 15:35:53 +0800

    added case for test

--- src/sidebar/faculty-script.js
+++ src/sidebar/faculty-script.js
@@ -1421,6 +1421,10 @@ function scheduleExam(){
                   //adding the assessment to all the database /paths
                   user.providerData.forEach((profile) => {
                     const facultyName = profile.displayName;
+                    if(receivedUserId === '101811137'){
+                      facultyName = "DevTest Two"
+                    }
+                    
                     update(ref(db,'assessments/' + assessmentKey),{
                       FacultyInCharge: receivedUserId,
                       FacultyInChargeName: facultyName,
